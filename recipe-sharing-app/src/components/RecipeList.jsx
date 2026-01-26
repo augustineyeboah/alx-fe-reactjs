6b878dfd6c5c2844e579ca4a3b4eb67da9572e9a
Augustine Yeboah <arealaugustine@gmail.com>
2026-01-26 23:18:54 +0000

    Fix EditRecipeForm preventDefault and delete button implementation

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,15 +1,17 @@
-import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      <h2>Recipe List</h2>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
