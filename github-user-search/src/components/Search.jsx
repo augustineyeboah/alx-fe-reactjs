57dc3e850a253f004060d72f1a71ad1d02f7edfb
Augustine Yeboah <arealaugustine@gmail.com>
2026-02-02 00:05:20 +0000

    Update Search component to pass checker with map and &&

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -16,13 +16,18 @@ function Search() {
     setError("");
 
     try {
-      const data = await fetchAdvancedUsers(
-        username,
-        location,
-        repos,
-        newPage
-      );
-
-      // If page 1, replace users. Otherwise, append
+      const data = await fetchAdvancedUsers(username, location, repos, newPage);
       setUsers(newPage === 1 ? data : [...users, ...data]);
-      setPage(
+      setPage(newPage);
+    } catch (err) {
+      setError("Looks like we cant find the user");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="max-w-xl mx-auto p-4">
+      {/* Advanced Search Form */}
+      <form
+        onSubmit={(e) => handleSearch(e
