c927368dd858cd2c63e745c0fe960f6e57f0df3c
Augustine Yeboah <arealaugustine@gmail.com>
2026-02-02 01:36:30 +0000

    Task 2: Advanced GitHub User Search with Tailwind UI

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { fetchAdvancedUsers } from "../services/githubService";
+import { fetchUserData } from "../services/githubService";
 
 function Search() {
   const [username, setUsername] = useState("");
