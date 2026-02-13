eb9d22f4dd5ea83cf2a8480ea3ced87475bcaa12
Augustine Yeboah <arealaugustine@gmail.com>
2026-02-13 01:45:09 +0000

    Fix Tailwind configuration format

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
